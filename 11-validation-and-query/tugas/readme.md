c6831897dd805f69a5fc01aee390ac7ce2adc3f5
Perdana Main <firman.fp123@gmail.com>
2024-12-17 23:46:34 +0700

    feat: add day 11

---
+++ 11-validation-and-query/tugas/readme.md
@@ -0,0 +1,5 @@
+##
+
+To: Kang Agung
+
+Mohon maaf kang saya submit package.json dulu sementara. Selanjutnya submit seluruh tugas saya. Terima kasih
--- 9-mongodb/latihan/src/index.ts
+++ 9-mongodb/latihan/src/index.ts
@@ -14,7 +14,7 @@ async function init() {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use("/api", routes);
+    app.use("/api/v1/", routes);
 
     app.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}`);
